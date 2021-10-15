3471c96865671b0cb0472304ef11f1f04d5b1c25
Janne Cools <janne.cools@ugent.be>
2021-10-15 10:37:05 +0200

    Toevoeging lege klassen

---
+++ src/oplossing/OptimalTree.java
@@ -0,0 +1,50 @@
+package oplossing;
+
+import opgave.Node;
+import opgave.OptimizableTree;
+import java.util.Iterator;
+import java.util.List;
+
+public class OptimalTree implements OptimizableTree {
+
+
+    @Override
+    public void optimize(List keys, List weights) {
+
+    }
+
+    @Override
+    public void optimize(List keys, List internalWeights, List externalWeights) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean search(Comparable o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(Comparable o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Comparable comparable) {
+        return false;
+    }
+
+    @Override
+    public Node root() {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
---
+++ src/oplossing/SemiSplayTree.java
@@ -0,0 +1,38 @@
+package oplossing;
+
+import opgave.Node;
+import opgave.SearchTree;
+import java.util.Iterator;
+
+public class SemiSplayTree implements SearchTree {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean search(Comparable o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(Comparable o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Comparable comparable) {
+        return false;
+    }
+
+    @Override
+    public Node root() {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
